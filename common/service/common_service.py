3cec8d6a24a0e92812d2d5fce197942f409cc77e
bangjaehyuk <xcrew1@gmail.com>
2020-03-18 11:05:20 +0900

    file. delete

--- common/service/common_service.py
+++
@@ -1,2 +0,0 @@
-def get_response_code(code):
-    return {'code': code.value, 'message': code.name}
--- reseller/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('main/', views.Main.as_view()),
-    path('userinfo/', views.UserInfo.as_view()),
-    path('logout/', views.Logout.as_view()),
-]
